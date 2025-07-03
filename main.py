f67b081e617f76cad4065800059a10c943d2da79
Kostiantyn Skliar <skljar.k@gmail.com>
2025-07-03 22:40:42 +0200

    Fixing main.py.

--- main.py
+++ main.py
@@ -2,35 +2,35 @@ from my_select import select_1, select_2, select_3, select_4, select_5, select_6
 
 
 if __name__ == "__main__":
-    print("Top 5 students by average grade:")
+    print(" #1 - Top 5 students by average grade:")
     for row in select_1():
         print(row)
 
-    print("\nTop student for subject 1:")
+    print("\n #2 - Top student for subject 1:")
     print(select_2(subject_id=1))
 
-    print("\nAverage grade per group for subject 1:")
+    print("\n #3 - Average grade per group for subject 1:")
     for row in select_3(subject_id=1):
         print(row)
 
-    print("\nAverage grade across all grades:")
+    print("\n #4 - Average grade across all grades:")
     print(select_4())
 
-    print("\nCourses taught by teacher 1:")
-    print(select_5(teacher_id=1))
+    print("\n #5 - Courses taught by teacher 2:")
+    print(select_5(teacher_id=2))
 
-    print("\nStudents in group 1:")
+    print("\n #6 - Students in group 1:")
     print(select_6(group_id=1))
 
-    print("\nGrades for students in group 1 for subject 1:")
+    print("\n #7 - Grades for students in group 1 for subject 1:")
     for row in select_7(group_id=1, subject_id=1):
         print(row)
 
-    print("\nAverage grade given by teacher 1:")
-    print(select_8(teacher_id=1))
+    print("\n #8 - Average grade given by teacher 2:")
+    print(select_8(teacher_id=2))
 
-    print("\nSubjects attended by student 1:")
+    print("\n #9 - Subjects attended by student 1:")
     print(select_9(student_id=1))
 
-    print("\nSubjects for student 1 taught by teacher 1:")
-    print(select_10(student_id=1, teacher_id=1))
+    print("\n #10 - Subjects for student 1 taught by teacher 2:")
+    print(select_10(student_id=1, teacher_id=2))
